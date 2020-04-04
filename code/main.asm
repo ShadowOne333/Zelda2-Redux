9cda53af8acfe4c3dbde65380af81f5f1c745fb5
ShadowOne333 <shadow_one666@hotmail.com>
2020-04-03 17:01:22 -0700

    Streamlined optional patches compilation

--- code/main.asm
+++ code/main.asm
@@ -34,8 +34,44 @@ incsrc code/graphics.asm		// Sprite/graphic changes
 incsrc code/title_screen.asm		// Title screen visual changes
 incsrc code/tunic_colors.asm		// Make blue tunic more vivid
 
+
 //****************************************
 // Optional patches
+// Uncomment the desired Optional patches
 //****************************************
 
-//incsrc code/optional/optional.asm	// Optional patches for Zelda Redux
+// Famicom Disk System version King's Tomb layout
+//incsrc code/optional/FDSKingsTomb.asm
+
+// Keep Experience Points after Game Over
+//incsrc code/optional/KeepExpAtGameOver.asm
+
+// New Experience system
+//incsrc code/optional/NewExp.asm
+
+// Original Zelda 2 behaviour when obtaining Link Dolls
+//incsrc code/optional/OriginalLinkDolls.asm
+
+// Original Zelda 2 behaviour when getting a Game Over 
+//incsrc code/optional/OriginalRestartAtGameOver.asm
+
+// Original Zelda 2 magic consumption
+//incsrc code/optional/OriginalSpellMagicConsumption.asm
+
+// Original Zelda 2 title screen
+//incsrc code/optional/OriginalTitleScreen.asm
+
+// Reduce lag in-game - Reduces number of enemies on-screen
+//incsrc code/optional/ReduceLag.asm
+
+// Remove the low health beeping
+//incsrc code/optional/RemoveLowHealthBeep.asm
+
+// Restart on the same screen after a Game Over
+//incsrc code/optional/RestartSameScreenAtGameOver.asm
+
+// Secret... ???
+//incsrc code/optional/Secret.asm
+
+// Static experience points while placing each Palace Crystal
+//incsrc code/optional/StaticExpAtPalaceCrystal.asm
