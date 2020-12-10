2d23788923f136d683807a9777b0fad23b83ae9a
ShadowOne333 <shadow_one666@hotmail.com>
2020-12-10 10:05:35 -0700

    Fix the 'Change Minor Item SFX' optional patch

--- code/optional/ChangeMinorItemSound.asm
+++ code/optional/ChangeMinorItemSound.asm
@@ -1,10 +1,15 @@
 //****************************************************************
-//	Change the Text printing sound from Dialog boxes
+//    Change the Sound that plays when grabbing a minor item
 //****************************************************************
 
 bank 7;
-// Sound for Text printing per-character
+// Sound for Items already available in-ground
 org $E802	// 0x1E812
 	lda.b #$02	// Originally LDA #$08
 	sta.b $EC	// Originally STA $EF, SFX Type 4
 // Changed SFX Type from 1 to 3
+
+// Sound for Item drops from enemies
+org $E847	// 0x1E857
+	lda.b #$02	// Originally LDA #$08
+	sta.b $EC	// Originally STA $EF, SFX Type 4
