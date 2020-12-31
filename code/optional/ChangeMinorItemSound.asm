4b2fdbc041fec1b29312e24141f5bb5ac2c803bd
ShadowOne333 <shadow_one666@hotmail.com>
2020-12-30 17:14:16 -0700

    Yet another missing code instance for the Minor Item SFX optional patch

--- code/optional/ChangeMinorItemSound.asm
+++ code/optional/ChangeMinorItemSound.asm
@@ -3,11 +3,16 @@
 //****************************************************************
 
 bank 7;
-// Sound for Items already available in-ground
+// Sound for Items already available in-ground inside Temples
+org $E79A	// 0x1E7AA
+	lda.b #$02	// Originally LDA #$08
+	sta.b $EC	// Originally STA $EF, SFX Type 4
+// Changed SFX Type from 4 to 1
+
+// Sound for Items already available in-ground in Overworld
 org $E802	// 0x1E812
 	lda.b #$02	// Originally LDA #$08
 	sta.b $EC	// Originally STA $EF, SFX Type 4
-// Changed SFX Type from 1 to 3
 
 // Sound for Item drops from enemies
 org $E847	// 0x1E857
